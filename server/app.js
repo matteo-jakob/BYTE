9df369d8f48e3d6ba5ac47c581a90c8e635799fd
Matteo <M.Jakob.Inf21@stud.bbbaden.ch>
2024-03-04 22:01:36 +0100

    pics

--- server/app.js
+++ server/app.js
@@ -18,38 +18,17 @@ var warenkorb = [];
 const waren = [
   {
     id: 0,
-    item: "Potatoes",
-    price: 10,
+    item: "Pizza Margherita",
+    price: 20,
     image_url:
-      "https://cdn.pixabay.com/photo/2016/08/11/08/43/potatoes-1585060_1280.jpg",
+      "https://pixabay.com/static/frontend/3c346409d336d5f09a7f.svg",
   },
   {
     id: 1,
-    item: "Rice",
-    price: 8,
+    item: "Pizza Salami",
+    price: 24,
     image_url:
-      "https://cdn.pixabay.com/photo/2019/02/15/03/28/rice-3997767_1280.jpg",
-  },
-  {
-    id: 2,
-    item: "Flour",
-    price: 10,
-    image_url:
-      "https://cdn.pixabay.com/photo/2020/05/12/17/54/rolling-pin-5164240_960_720.jpg",
-  },
-  {
-    id: 3,
-    item: "Water",
-    price: 3,
-    image_url:
-      "https://cdn.pixabay.com/photo/2018/07/09/15/20/desire-3526366_960_720.jpg",
-  },
-  {
-    id: 4,
-    item: "Cola",
-    price: 4.5,
-    image_url:
-      "https://cdn.pixabay.com/photo/2016/11/29/13/49/bottle-1869990_960_720.jpg",
+      "https://cdn.pixabay.com/photo/2021/09/02/13/26/salami-pizza-6593465_960_720.jpg",
   },
 ];
 
